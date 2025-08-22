2d1710a024af2ecf3094454fb928961f1ceff010
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-08-22 17:03:06 -0500

    first commit

---
+++ backend/app/models/reservas.js
@@ -0,0 +1,17 @@
+// Modelo para la gestión de pedidos.
+// Aquí se define la estructura y configuración para la tabla de pedidos en la base de datos.
+// Puedes modificar la estructura, nombres de campos o lógica según la temática o cambios futuros en el proyecto.
+
+import mongoose from "mongoose";
+
+const pedidoSchema = new mongoose.Schema({
+  codigo: { type: Number, required: true, unique: true },
+  descripcion: { type: Number, required: true },
+  id_cliente: { type: String, required: true, maxlength: 20 },
+  id_vehiculo: { type: Number, required: true, default: 1 },
+  estado: { type: String, enum: ['pendiente', 'reserva aceptada', 'no disponible', 'reserva eliminada'], default: 'pendiente' }
+});
+
+const Pedido = mongoose.model("Pedido", pedidoSchema);
+
+export default Pedido;
---
+++ backend/app/models/vehiculos.js
@@ -0,0 +1,20 @@
+// Modelo para la gestión de productos.
+// Aquí se define la estructura y configuración para la tabla de productos en la base de datos.
+// Puedes modificar la estructura, nombres de campos o lógica según la temática o cambios futuros en el proyecto.
+
+import mongoose from "mongoose";
+
+const productoSchema = new mongoose.Schema({
+  marca: { type: String, required: true, maxlength: 20 },
+  modelo: { type: String, required: true, unique: true, maxlength: 20 },
+  anio_fabricacion: { type: Date },
+  placa: { type: String, maxlength: 7 },
+  color: { type: String },
+  tipo: { type: String },
+  kilometraje: { type: String },
+  descripcion: { type: String, maxlength: 20 }
+});
+
+const Producto = mongoose.model("Producto", productoSchema);
+
+export default Producto;
